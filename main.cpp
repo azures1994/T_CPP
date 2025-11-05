08ebe00501f0c8a4be585f0aeca82ae4b0ed66de
Azures <azures1994@163.com>
2025-11-05 10:42:51 +0800

    Init Repo.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from T_CPP!\n";
+
+    return 0;
+}
---
+++ src/Temp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "config.h"
+
+int main(int argc, char* argv[]){
+
+    printf("====== %s ======\n", PROJECT_NAME.c_str());
+
+    return 0;
+}
